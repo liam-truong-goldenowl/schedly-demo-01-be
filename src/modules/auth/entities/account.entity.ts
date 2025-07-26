5df107b66e7783f01d27efe86b016aabea1dd7b1
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-26 15:43:56 +0700

    feat: user sign-up with email and password

--- src/modules/auth/entities/account.entity.ts
+++ src/modules/auth/entities/account.entity.ts
@@ -2,11 +2,14 @@ import {
   Entity,
   OneToOne,
   Property,
+  BeforeCreate,
+  BeforeUpdate,
   EntityRepositoryType,
 } from '@mikro-orm/postgresql';
 
 import { BaseEntity } from '@/common/entities/base.entity';
 import { User } from '@/modules/user/entities/user.entity';
+import { verifyHash, generateHash } from '@/utils/helpers/hash';
 
 import { AccountRepository } from '../account.repository';
 
@@ -15,8 +18,37 @@ export class Account extends BaseEntity {
   [EntityRepositoryType]?: AccountRepository;
 
   @Property()
-  passwordHash: string;
+  private passwordHash: string;
 
   @OneToOne(() => User)
   user: User;
+
+  /**
+   * The raw password provided by the user.
+   */
+  private password: string;
+  /**
+   * Indicates if the password has been modified since the last hash.
+   */
+  private passwordDirty = false;
+
+  setPassword(raw: string) {
+    this.password = raw;
+    this.passwordDirty = true;
+  }
+
+  @BeforeCreate()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (!this.passwordDirty) {
+      return;
+    }
+
+    this.passwordHash = await generateHash({ source: this.password });
+    this.passwordDirty = false;
+  }
+
+  async verifyPassword(raw: string): Promise<boolean> {
+    return verifyHash({ source: raw, hash: this.passwordHash });
+  }
 }
