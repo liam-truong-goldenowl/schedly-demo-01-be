c3a618d16242fd406164c0febea07122604a7aad
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-26 14:37:29 +0700

    refactor: update base entity created_at column

--- src/common/entities/base.entity.ts
+++ src/common/entities/base.entity.ts
@@ -1,15 +1,18 @@
 import {
   Property,
   PrimaryKey,
+  OptionalProps,
   BaseEntity as MikroOrmBase,
 } from '@mikro-orm/core';
 
-export abstract class BaseEntity extends MikroOrmBase {
-  @PrimaryKey()
-  id!: number;
+export abstract class BaseEntity<Optional = never> extends MikroOrmBase {
+  [OptionalProps]?: 'createdAt' | 'updatedAt' | Optional;
 
-  @Property()
-  createdAt = new Date();
+  @PrimaryKey()
+  id: number;
+
+  @Property({ onCreate: () => new Date() })
+  createdAt: Date;
 
   @Property({ nullable: true, onUpdate: () => new Date() })
   updatedAt?: Date;
