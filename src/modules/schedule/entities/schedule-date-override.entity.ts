d19018e4f6e567e63259cf6722a6c189649edcc4
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-29 17:31:14 +0700

    feat: create schedule date override table

---
+++ src/modules/schedule/entities/schedule-date-override.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Property, ManyToOne } from '@mikro-orm/core';
+
+import { BaseEntity } from '@/common/entities/base.entity';
+
+import { Schedule } from './schedule.entity';
+
+@Entity()
+export class ScheduleDateOverride extends BaseEntity {
+  @Property({ type: 'date' })
+  date: Date;
+
+  @Property({ type: 'time' })
+  startTime: string;
+
+  @Property({ type: 'time' })
+  endTime: string;
+
+  @ManyToOne(() => Schedule)
+  schedule: Schedule;
+}
