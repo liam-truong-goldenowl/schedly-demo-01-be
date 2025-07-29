d7ae2a9aaebe8f0e3a468ecdc9458c6309260cf0
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-29 16:46:30 +0700

    feat: create schedule table

--- src/modules/schedule/entities/schedule.entity.ts
+++ src/modules/schedule/entities/schedule.entity.ts
@@ -1,1 +1,19 @@
-export class Schedule {}
+import { Entity, Property, ManyToOne } from '@mikro-orm/postgresql';
+
+import { BaseEntity } from '@/common/entities/base.entity';
+import { User } from '@/modules/user/entities/user.entity';
+
+@Entity()
+export class Schedule extends BaseEntity {
+  @ManyToOne(() => User)
+  user: User;
+
+  @Property()
+  name: string;
+
+  @Property()
+  timezone: string;
+
+  @Property()
+  isDefault = true;
+}
