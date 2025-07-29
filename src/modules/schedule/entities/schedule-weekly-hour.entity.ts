42a015a07df0c3c9f6a7689e41439ec0286c324a
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-29 17:10:17 +0700

    feat: create schedule weekly hour table

---
+++ src/modules/schedule/entities/schedule-weekly-hour.entity.ts
@@ -0,0 +1,21 @@
+import { Enum, Entity, Property, ManyToOne } from '@mikro-orm/core';
+
+import { Weekday } from '@/common/enums';
+import { BaseEntity } from '@/common/entities/base.entity';
+
+import { Schedule } from './schedule.entity';
+
+@Entity()
+export class ScheduleWeeklyHour extends BaseEntity {
+  @Enum({ items: () => Weekday, nativeEnumName: 'weekday' })
+  weekday: Weekday;
+
+  @Property({ type: 'time' })
+  startTime: string; // e.g., '08:00'
+
+  @Property({ type: 'time' })
+  endTime: string; // e.g., '17:00'
+
+  @ManyToOne(() => Schedule)
+  schedule: Schedule;
+}
--- src/modules/schedule/schedule.module.ts
+++ src/modules/schedule/schedule.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 import { ScheduleService } from './schedule.service';
+import { Schedule } from './entities/schedule.entity';
 import { ScheduleController } from './schedule.controller';
+import { ScheduleWeeklyHour } from './entities/schedule-weekly-hour.entity';
 
 @Module({
+  imports: [MikroOrmModule.forFeature([Schedule, ScheduleWeeklyHour])],
   controllers: [ScheduleController],
   providers: [ScheduleService],
 })
