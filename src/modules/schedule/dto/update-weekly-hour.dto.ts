84f92364c0933b28b2d5f71bb5933fabc9419b0b
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-30 16:27:42 +0700

    feat: make startTime and endTime required to update weekly hour

--- src/modules/schedule/dto/update-weekly-hour.dto.ts
+++ src/modules/schedule/dto/update-weekly-hour.dto.ts
@@ -1,7 +1,7 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
 
 import { CreateWeeklyHourDto } from './create-weekly-hour.dto';
 
-export class UpdateWeeklyHourDto extends PartialType(
-  OmitType(CreateWeeklyHourDto, ['weekday']),
-) {}
+export class UpdateWeeklyHourDto extends OmitType(CreateWeeklyHourDto, [
+  'weekday',
+]) {}
