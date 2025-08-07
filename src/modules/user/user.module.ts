804699e29821c77942e0fa8fe1dc8fa681e17fb4
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-08-07 12:14:44 +0700

    refactor: update the user service to use new uuid module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 
+import { UUIDModule } from '../uuid/uuid.module';
+
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 
 @Module({
   exports: [UserService],
   providers: [UserService],
-  imports: [MikroOrmModule.forFeature([User])],
+  imports: [MikroOrmModule.forFeature([User]), UUIDModule],
 })
 export class UserModule {}
